7285d250e0b309b1c99af293a57e0e2e8aeccc64
saikumar22 <saigoud2212@gmail.com>
2020-08-17 22:07:43 +0530

    FSJ0720A116

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import utility.ConnectionManager;
 
 @WebServlet(urlPatterns= {"/signup"})
 public class SignUpController extends HttpServlet {
@@ -27,6 +27,8 @@ public class SignUpController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Sign up Controller");
 		// Fill your code here
+		ConnectionManager cm = new ConnectionManager();
+		cm.getConnection();	
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
 		rd.forward(request,response);
 	}
